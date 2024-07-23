d9e18058a10448595372519b5c7bafb1dc5e55b4
Paraew <66011297e@kmitl.ac.th>
2024-07-23 15:38:55 +0700

    feature

---
+++ string_operations.py
@@ -0,0 +1,24 @@
+
+#**Assignment 6: Importing Modules in Python**
+#Import and use modules in Python to enhance functionality. 
+# #Instructions:
+
+#1. Create a new Python file named `string_operations.py`.
+
+#2. Define functions for common string operations:
+# string_operations.py
+
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_string(s):
+    return s.capitalize()
+
+def lowercase_string(s):
+    return s.lower()
+
+def uppercase_string(s):
+    return s.upper()
+
+
+#3. Create another Python file named `main.py`.
